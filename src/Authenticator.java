da6a4f8d1c93f662800dc96abb02e78d97116f42
hatPenguinMan16 <hatman07@proton.me>
2026-01-09 09:38:30 +0100

    Commit

--- src/Authenticator.java
+++ src/Authenticator.java
@@ -1,8 +1,7 @@
 public class Authenticator {
    //private User[] UserList; no need for list, only one user at a time (at this point in the project)
 
-
-
+    /*
     public void isValidUser(String inputName, String inputPassword, int currentUser){
         if (UserList[currentUser].getName() == inputName && UserList[currentUser].getPassword() == inputPassword){
             System.out.println("User is valid!");
@@ -10,6 +9,6 @@ public class Authenticator {
         else{
             System.out.println("Bad username or password!");
         }
-    }
+    }*/
 
 }

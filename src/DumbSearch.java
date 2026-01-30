138a7d61cd9559bd2729abe40def41983d5feaee
hatPenguinMan16 <hatman07@proton.me>
2026-01-30 09:41:19 +0100

    old search fixed

--- src/DumbSearch.java
+++ src/DumbSearch.java
@@ -22,12 +22,12 @@ public class DumbSearch implements SearchMotor{
         }
 
         for (int idx = 0; idx < this.bookList.size(); idx++) {
-            if (this.bookList.get(idx).getTitle().equalsIgnoreCase(searchMsg)) retBooks.add(this.bookList.get(idx));
-            if (this.bookList.get(idx).getAuthor().equalsIgnoreCase(searchMsg)) retBooks.add(this.bookList.get(idx));
-            if (this.bookList.get(idx).getLanguage().equalsIgnoreCase(searchMsg)) retBooks.add(this.bookList.get(idx));
-            if (this.bookList.get(idx).getYear().equals(searchMsg)) retBooks.add(this.bookList.get(idx));
-            if (this.bookList.get(idx).getIsbn().equals(searchMsg)) retBooks.add(this.bookList.get(idx));
-            if (this.bookList.get(idx).getBorrowed().equalsIgnoreCase(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getTitle().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getAuthor().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getLanguage().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getYear().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getIsbn().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
+            if (this.bookList.get(idx).getBorrowed().contains(searchMsg)) retBooks.add(this.bookList.get(idx));
         }
     }
 }
